634e1e03ee0e770d3682eb2705fe01996baf9618
Kratory <apazgarcia17@outlook.es>
2019-01-23 10:22:24 +0100

    opcional

--- Tema 5/Ejercicio5/src/Ejercicio5.java
+++ Tema 5/Ejercicio5/src/Ejercicio5.java
@@ -6,18 +6,28 @@ import java.util.Scanner;
 public class Ejercicio5 {
     public static void main(String[] args){
         char continuar;
-        do{
-            System.out.println("Introduzca un año para comprobar sus ventas");
+        
             Scanner sc = new Scanner(System.in);
-            int ano = Integer.parseInt(sc.nextLine());
+            if(args.length == 0){ // Si no se le pasa ningun parametro por linea de comados.
+                do{
+                    System.out.println("Introduzca un año para comprobar sus ventas");
+                    int ano = Integer.parseInt(sc.nextLine());
+                    Ventas v = new Ventas(ano);
 
-            Ventas v = new Ventas(ano);
-            v.grafica();
-            System.out.printf("Media de ventas: %.1f\n", v.media());
+                    v.grafica();
+                    System.out.printf("Media de ventas: %.1f\n", v.media());
+
+                    System.out.println("¿Continuar? (y/n)");
+                    continuar = sc.nextLine().charAt(0);
+                }while(continuar == 'y');
+            }
+            else { // Si se le pasa el parametro por linea de comandos.
+                Ventas v = new Ventas(Integer.parseInt(args[0]));
+                v.grafica();
+                System.out.printf("Media de ventas: %.1f\n", v.media());
+            }
             
-            System.out.println("¿Continuar? (y/n)");
-            continuar = sc.nextLine().charAt(0);
-        }while(continuar == 'y');
+        
         
     }
 }
